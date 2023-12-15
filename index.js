0c52b56962b434fb8ed6f72be62416d64397d18e
rachanapriya1 <139256685+rachanapriya1@users.noreply.github.com>
2023-12-15 20:29:20 +0530

    index.js

--- index.js
+++ index.js
@@ -185,58 +185,58 @@ function brackets(expression) {
 
 // Q9. Write a program to reverse a stack.
 
-// class Stack {
-//     constructor() {
-//         this.top = null
-//     }
-//     push(ele) {
-//         var node = new newNode(ele)
-//         node.next = this.top
-//         this.top = node
-//     }
-//     pop() {
-//         var temp = this.top
-//         var char = temp.data
-//         this.top = this.top.next
-//         temp = null
-//         return char
-//     }
-//     reverseString(str) {
-//         var i = 0
-//         var reversestr = ""
-//         while (i != str.length) {
-//             this.push(str.charAt(i)
-// )
-//             i++
-//         }
-//         var temp = this.top
-//         while (temp != null) {
-//             var char
-//             char = this.pop()
-//             reversestr += char
-//             temp = this.top
-//         }
-//         return reversestr
-//     }
-//     display() {
-//         var temp = this.top
-//         while (temp != null) {
-//             console.log(temp.data)
-//             temp = temp.next
-//         }
-//     }
-// }
-// class newNode {
-//     constructor(data, next) {
-//         this.data = data
-//         this.next = null
-//     }
-// }
+class Stack {
+    constructor() {
+        this.top = null
+    }
+    push(ele) {
+        var node = new newNode(ele)
+        node.next = this.top
+        this.top = node
+    }
+    pop() {
+        var temp = this.top
+        var char = temp.data
+        this.top = this.top.next
+        temp = null
+        return char
+    }
+    reverseString(str) {
+        var i = 0
+        var reversestr = ""
+        while (i != str.length) {
+            this.push(str.charAt(i)
+)
+            i++
+        }
+        var temp = this.top
+        while (temp != null) {
+            var char
+            char = this.pop()
+            reversestr += char
+            temp = this.top
+        }
+        return reversestr
+    }
+    display() {
+        var temp = this.top
+        while (temp != null) {
+            console.log(temp.data)
+            temp = temp.next
+        }
+    }
+}
+class newNode {
+    constructor(data, next) {
+        this.data = data
+        this.next = null
+    }
+}
 
-// const stack = new Stack()
-// const string = "Rachana"
-// const reverse = stack.reverseString(string)
-// console.log(`The String provided - ${string}\nString in reverse format -${reverse}`)
+const stack = new Stack()
+const string = "Rachana"
+const reverse = stack.reverseString(string)
+console.log(`The String provided - ${string}\nString in reverse format -${reverse}`)
 
 // Q10. Write a program to find the smallest number using a stack.
 
